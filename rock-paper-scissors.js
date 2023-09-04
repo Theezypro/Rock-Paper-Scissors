57f9aabfbd562de5faf3cbd777c82909dc84def8
TOLULOPE BADEJO <badetolu11@gmail.com>
2023-09-04 19:09:38 +0100

    PNG

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -93,8 +93,8 @@ function playGame(playerMove) {
   document.querySelector(".js-result").innerHTML = result;
 
   document.querySelector(".js-moves").innerHTML = ` You
-  <img src="images/${playerMove}-emoji.png" class="move-icon" />
-  <img src="images/${computerMove}-emoji.png" class="move-icon" />
+  <img src="images/${playerMove}-emoji.PNG" class="move-icon" />
+  <img src="images/${computerMove}-emoji.PNG" class="move-icon" />
   Computer`;
 }
 
